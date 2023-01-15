7d18a21e9e92fa8249516e135dde2035692abb95
Anna Luiza <luizafistarol@gmail.com>
2023-01-15 15:38:08 -0300

    Armazenando valores do input do modal em variáveis

--- js/main.js
+++ js/main.js
@@ -1,22 +1,75 @@
-//@ FUNÇÃO PARA ABRIR O MODAL
+//const newDate = new Date(dateUser)
+//console.log(newDate.toLocaleString("pt-BR", {timeZone: "UTC"})) //@ CONVERTER DATA EM TIMESTAMP PARA DATA
+
+let usuarios = {}
+
+const nomeUser = document.getElementById("nome-user")
+const stringDateUser = document.getElementById("date")
+
+
+//@ FUNÇÃO PARA ABRIR O MODAL - adicionar
 const modal = document.getElementById("modal-overlay")
 const modalAdicionar = document.getElementById("add")
 const modalEditar = document.getElementById("edit")
-const nomeUser = document.getElementById("nome-user")
 
 function abrirModalAdicionar(){
+    const btnAdicionar = document.getElementById("btn-adicionar");
+    const pAdicionar = document.getElementById("p-adicionar");    
+    
+    btnAdicionar.addEventListener("click", adicionarPerfil)
+    pAdicionar.addEventListener("click", adicionarPerfil)
+}
+abrirModalAdicionar()
+
+function adicionarPerfil(){
+    //Exibir modal
     modal.style.display = "flex"
     modalAdicionar.style.display = "inline"
     modalEditar.style.display = "none"
+
+    //Adicionar perfil
+    //const nomeUser = 
+    console.log(this)
+    console.log(modal)
+    modalAdicionar.addEventListener("click", (event) => {
+        const nomeUserNovo = nomeUser.value
+        const dateUser = Date.parse(stringDateUser.value)
+        criarLi(nomeUserNovo, dateUser)
+    })
 }
 
-console.log(nomeUser.textContent)
+//@ CRIAR AS LI'S
+
+function criarLi(nomeUser, dateUser){
+    console.log(ul)
+    console.log(nomeUser)
+    console.log(dateUser)
+
+    //criando a li =
+    const li = document.createElement("li")
+    li.className = "usuario"
+}
+
+
+
+
+
+
+//@ FUNÇÃO PARA EDITAR O MODAL
 function abrirModalEditar(nomeUserAtual){
     modal.style.display = "flex"
     modalAdicionar.style.display = "none"
     modalEditar.style.display = "inline"
-    nomeUser.value = nomeUserAtual
- 
+    //nomeUser.value = nomeUserAtual
+    //console.log(nomeUser.textContent)
+    //nomeUser.nomeUser = ""
+    //modalEditar.addEventListener("click", (event) => {
+    //    const novoNomeUser = nomeUser.value
+    //    modal.style.display = "none"
+    //    console.log(novoNomeUser)
+    //    console.log(event.target)
+    //    console.log(modalEditar)
+    //})
 }
 
 //@ FUNÇÃO PARA FECHAR O MODAL
@@ -31,14 +84,7 @@ function fecharModal() {
 fecharModal()
 
 //@FUNÇÃO PARA CHAMAR O MODAL
-function adicionarPerfil(){
-    const btnAdicionar = document.getElementById("btn-adicionar");
-    const pAdicionar = document.getElementById("p-adicionar");    
-    
-    btnAdicionar.addEventListener("click", abrirModalAdicionar)
-    pAdicionar.addEventListener("click", abrirModalAdicionar)
-}
-adicionarPerfil()
+
 
 function editarPerfil(){
     const btnsEdit = [...document.getElementsByClassName("fa-edit")];

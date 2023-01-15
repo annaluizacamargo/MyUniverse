babc5bb59c9e56bc966a5dc7e89adc7fe8560479
Anna Luiza <luizafistarol@gmail.com>
2023-01-14 21:48:17 -0300

    Iniciando declaração de variáveis no JavaScript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+//@ FUNÇÃO PARA ABRIR O MODAL
+function abrirModal(){
+    console.log("abrir modal")
+}
+abrirModal()
+
+//@FUNÇÃO PARA CHAMAR O MODAL
+function adicionarEditarPerfil(){
+    const btnEdit = document.getElementsByClassName("fa-edit");
+    const btnAdicionar = document.getElementById("btn-adicionar");
+    const pAdicionar = document.getElementById("p-adicionar");
+    console.log(btnEdit)
+    console.log(btnAdicionar)
+    console.log(pAdicionar)
+
+}
+adicionarEditarPerfil()
+
+//@FUNÇÃO PARA CRIAR LI'S
+const ul = document.getElementById("usuarios");
+console.log(ul)
+
+//@FUNÇÃO PARA ABRIR PÁGINA API
+const lis = [...document.getElementsByTagName("li")]
+console.log(lis)
+
+
+
 console.log("MyUniverse")
+
+
+
+

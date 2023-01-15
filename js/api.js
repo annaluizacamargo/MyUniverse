7192500aa7704d66200cc8a73174a822e5d48f89
Anna Luiza <luizafistarol@gmail.com>
2023-01-15 20:56:04 -0300

    Adicionando arquivo de js para integração com api

---
+++ js/api.js
@@ -0,0 +1,3 @@
+//const newDate = new Date(dateUser)
+//console.log(newDate.toLocaleString("pt-BR", {timeZone: "UTC"})) //@ CONVERTER DATA EM TIMESTAMP PARA DATA
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.1/axios.min.js"></script>
--- js/main.js
+++ js/main.js
@@ -193,7 +193,3 @@ btnsUser.forEach((btnUser) => {
         window.location.href = '/MyUniverse/result-api.html';
     })
 })
-
-//const newDate = new Date(dateUser)
-//console.log(newDate.toLocaleString("pt-BR", {timeZone: "UTC"})) //@ CONVERTER DATA EM TIMESTAMP PARA DATA
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.1/axios.min.js"></script>

8086f66e0216b575c71fc34f3ba5f1772ea07145
Anna Luiza <luizafistarol@gmail.com>
2023-01-16 18:17:01 -0300

    Capturando os dados da API da NASA

--- js/api.js
+++ js/api.js
@@ -11,3 +11,12 @@ const dadosLocalStorageConvertidos = JSON.parse(dadosLocalStorage); //Transforma
 const nameUser = dadosLocalStorageConvertidos.nomeUser;//Capturando o nome/apelido do usuário
 const dateUser = new Date(dadosLocalStorageConvertidos.dateUser); //Capturando a data em timestamp e convertendo em data por extenso
 const date = dateUser.toLocaleDateString('en-CA', {timeZone: "UTC"}); //Convertendo a data para o modelo aceito pela API (aaaa-mm-dd)
+
+async function buscaImagemNASA(date){
+    const apiKey = "bkk31nnfNbtwuzjWOiF5GZKs2IGyhE4HVEANlUcy";
+    let consultaData = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`)
+    let consultaDataConvertida = await consultaData.json()
+    console.log(consultaDataConvertida)
+    console.log("fim")
+}
+buscaImagemNASA(date)
